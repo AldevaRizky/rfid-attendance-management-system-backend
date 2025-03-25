fca48700cfcc0aebcce288d980779c7c8d72b6f8
Aldeva Rizky <aldevarwpp@gmail.com>
2025-03-25 20:47:27 +0700

    feat(user): add User model with relationships and authentication support

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,36 +10,44 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'name', 'email', 'password', 'nip', 'role', 'division_id', 'position_id', 
+        'education_id', 'rfid_card_id', 'phone_number', 'gender', 'birth_date', 
+        'birth_place', 'city', 'address', 'join_date', 'status', 'profile_picture', 
+        'last_login_at'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    protected $hidden = ['password'];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
+    public function division()
+    {
+        return $this->belongsTo(Division::class);
+    }
+
+    public function position()
+    {
+        return $this->belongsTo(Position::class);
+    }
+
+    public function education()
+    {
+        return $this->belongsTo(Education::class);
+    }
+
+    public function rfidCard()
+    {
+        return $this->belongsTo(RfidCard::class);
+    }
+
+    public function attendanceLogs()
+    {
+        return $this->hasMany(AttendanceLog::class);
+    }
+
+    public function leaveRequests()
+    {
+        return $this->hasMany(LeaveRequest::class);
+    }
 }
