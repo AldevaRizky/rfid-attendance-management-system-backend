fdd18a3e100b41785bc13c8ff2b082cf1fdbad8b
Aldeva Rizky <aldevarwpp@gmail.com>
2025-03-25 21:46:07 +0700

    feat(auth): install Jetstream with Livewire and update migrations and model

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,48 +6,106 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
+    use HasApiTokens;
     use HasFactory;
+    use HasProfilePhoto;
+    use Notifiable;
+    use TwoFactorAuthenticatable;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
     protected $fillable = [
-        'name', 'email', 'password', 'nip', 'role', 'division_id', 'position_id', 
-        'education_id', 'rfid_card_id', 'phone_number', 'gender', 'birth_date', 
-        'birth_place', 'city', 'address', 'join_date', 'status', 'profile_picture', 
-        'last_login_at'
+        'name',
+        'email',
+        'password',
+        'nip', // Added 'nip' to fillable
+        'role', // Added 'role' to fillable
+        'division_id',
+        'position_id',
+        'education_id',
+        'rfid_card_id',
+        'phone_number',
+        'gender',
+        'birth_date',
+        'birth_place',
+        'city',
+        'address',
+        'join_date',
+        'status',
+        'last_login_at',
     ];
 
-    protected $hidden = ['password'];
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+        'two_factor_recovery_codes',
+        'two_factor_secret',
+    ];
 
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'birth_date' => 'date',
+        'join_date' => 'date',
+        'last_login_at' => 'datetime',
+    ];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array<int, string>
+     */
+    protected $appends = [
+        'profile_photo_url',
+    ];
+
+    /**
+     * Relationship with the Division model.
+     */
     public function division()
     {
         return $this->belongsTo(Division::class);
     }
 
+    /**
+     * Relationship with the Position model.
+     */
     public function position()
     {
         return $this->belongsTo(Position::class);
     }
 
+    /**
+     * Relationship with the Education model.
+     */
     public function education()
     {
         return $this->belongsTo(Education::class);
     }
 
+    /**
+     * Relationship with the RfidCard model.
+     */
     public function rfidCard()
     {
-        return $this->belongsTo(RfidCard::class);
-    }
-
-    public function attendanceLogs()
-    {
-        return $this->hasMany(AttendanceLog::class);
-    }
-
-    public function leaveRequests()
-    {
-        return $this->hasMany(LeaveRequest::class);
+        return $this->belongsTo(RfidCard::class, 'rfid_card_id');
     }
 }
